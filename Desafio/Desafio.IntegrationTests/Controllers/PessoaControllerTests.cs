186fe9315027bf16aaff78e0b9dfaeee948437af
rfd-dev <rfd.dev@outlook.com>
2023-11-12 20:03:04 -0300

    feat: add integration testing projet

---
+++ Desafio/Desafio.IntegrationTests/Controllers/PessoaControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.IntegrationTests.Controllers
+{
+    public class PessoaControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public PessoaControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOnlySeededData()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/Pessoa");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
